253530871550b2fd99d252bcfb5640a912a7e46e
Naresh Kumar Reddy <gaddam.1987@gmail.com>
2017-02-25 00:17:11 +0100

    who am i for jenkins setup

--- dockerise.sh
+++ dockerise.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+whoami
 docker login -u gaddam1987 -p $DOCKER_PASSWORD
 docker build -t gaddam1987/mesos-marathon:$BUILD_NUMBER
 docker push gaddam1987/mesos-marathon:$BUILD_NUMBER
